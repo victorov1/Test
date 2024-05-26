0cb2aff889687dac8ecf024d144a4acfe833f3c8
victorov1 <u_victorov@mail.ru>
2024-05-26 21:05:15 +0500

    Test next commmit 2

--- src/Test.java
+++ src/Test.java
@@ -3,9 +3,6 @@ public class Test {// объявляется класс Test
    public static void main(String[] args) {// так включаются программы,
                                           // просто запомнить
  
-      System.out.print("Hello ");
-      System.out.println("world");
-      System.out.print("5 ");
-      System.out.print("раз");
+      System.out.print("Hello world 3 раза! ");
    }
 }
