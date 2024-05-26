cabcbfd7461d9ca1a07b3530e6ca721b81fe6b15
victorov1 <u_victorov@mail.ru>
2024-05-26 14:19:53 +0500

    Init

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {// объявляется класс Test
+ 
+   public static void main(String[] args) {// так включаются программы,
+                                          // просто запомнить
+ 
+      System.out.print("Hello ");
+      System.out.println("world");
+      System.out.print("5 ");
+      System.out.print("раз");
+   }
+}
